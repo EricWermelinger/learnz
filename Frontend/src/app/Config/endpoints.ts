1e9b10966e63fb884f052703f5595da6cffb39eb
Eric Wermelinger <eric.wermelinger@diartis.ch>
2022-07-04 15:38:07 +0200

    Sign Up Darktheme

--- Frontend/src/app/Config/endpoints.ts
+++ Frontend/src/app/Config/endpoints.ts
@@ -13,7 +13,7 @@ export const endpoints = {
     // TogetherConnectUser: 'togetherConnectUser',
     // TogetherSwipeUser: 'togetherSwipeUser',
     // TogetherUsers: 'togetherUsers',
-    // UserDarkTheme: 'userDarkTheme',
+    UserDarkTheme: 'userDarkTheme',
     UserLogin: 'userLogin',
     // UserProfile: 'userProfile',
     UserRefreshToken: 'userRefreshToken',
--- Frontend/src/app/Pages/sign-up/sign-up.service.ts
+++ Frontend/src/app/Pages/sign-up/sign-up.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
 import { endpoints } from 'src/app/Config/endpoints';
 import { TokenDTO } from 'src/app/DTOs/User/TokenDTO';
+import { UserDarkThemeDTO } from 'src/app/DTOs/User/UserDarkThemeDTO';
 import { UserSignUpDTO } from 'src/app/DTOs/User/UserSignUpDTO';
 import { ApiService } from 'src/app/Framework/API/api.service';
 import { TokenService } from 'src/app/Framework/API/token.service';
+import { DarkThemeService } from 'src/app/Framework/dark-theme/dark-theme.service';
 
 @Injectable({
   providedIn: 'root'
@@ -13,11 +15,17 @@ export class SignUpService {
   constructor(
     private api: ApiService,
     private tokenService: TokenService,
+    private darkThemeService: DarkThemeService,
   ) { }
 
   save(value: UserSignUpDTO) {
     this.api.callApi<TokenDTO>(endpoints.UserSignUp, value, 'POST').subscribe(token => {
       this.tokenService.patchToken(token);
     });
+  }
+
+  setDarkTheme(value: UserDarkThemeDTO) {
+    this.darkThemeService.setDarkTheme(value.darkTheme);
+    this.darkThemeService.applyDarkTheme(value.darkTheme);
   }
 }
