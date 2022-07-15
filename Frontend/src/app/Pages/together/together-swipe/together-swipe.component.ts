a5887d359aa5e8496f8073fe5fb11e30df074c07
Eric Wermelinger <eric.wermelinger@diartis.solutions>
2022-07-15 17:42:15 +0700

    Swipe Style

--- Frontend/src/app/Pages/together/together-swipe/together-swipe.component.ts
+++ Frontend/src/app/Pages/together/together-swipe/together-swipe.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { Observable } from 'rxjs';
 import { TogetherSwipeDTO } from 'src/app/DTOs/Together/TogetherSwipeDTO';
 import { TogetherUserProfileDTO } from 'src/app/DTOs/Together/TogetherUserProfileDTO';
+import { getGrades } from 'src/app/Enums/Grade';
+import { getSubjects } from 'src/app/Enums/Subject';
 import { TogetherSwipeService } from './together-swipe.service';
 
 @Component({
@@ -27,5 +29,13 @@ export class TogetherSwipeComponent {
     this.swipeService.swipe(value).subscribe(_ => {
       this.swipe$ = this.swipeService.getNextSwipe();
     });
+  }
+
+  translateSubject(subject: number) {
+    return 'Subject.' + getSubjects().filter(s => s.value === subject)[0].key;
+  }
+  
+  translateGrade(grade: number) {
+    return 'Grade.' + getGrades().filter(g => g.value === grade)[0].key;
   }
 }
--- Frontend/src/environments/environment.ts
+++ Frontend/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  IS_PROD: false,
+  IS_PROD: true,
   URL_API: 'https://localhost:7039/api/',
   URL_FRONTEND: 'http://localhost:4200/'
 };
