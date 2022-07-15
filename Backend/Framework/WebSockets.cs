e28f00cce1814d8b69cba7e3c8f3ab05b5850ad0
Eric Wermelinger <eric.wermelinger@diartis.solutions>
2022-07-15 18:05:00 +0700

    Web Socket Mapping

---
+++ Backend/Framework/WebSockets.cs
@@ -0,0 +1,7 @@
+﻿namespace Learnz.Framework;
+public class WebSockets
+{
+    public static string TogetherAsk = "together-ask";
+    public static string TogetherConnect = "together-connect";
+    public static string TogetherChat = "together-chat";
+}
