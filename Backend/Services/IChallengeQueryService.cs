bbaaec5ffb6d40d927e61b05f8b379e244f977ec
Eric Wermelinger <eric.wermelinger@diartis.ch>
2022-08-10 17:40:48 +0200

    Challenge Backend

---
+++ Backend/Services/IChallengeQueryService.cs
@@ -0,0 +1,12 @@
+﻿namespace Learnz.Services;
+public interface IChallengeQueryService
+{
+    Task TriggerWebsocket(Guid challengeId, Guid guid);
+    Task TriggerWebsocket(ChallengeActiveDTO challenge, Guid challengeId, Guid guid);
+    Task TriggerWebsocketAllUsers(Guid challengeId);
+    Task<ChallengeActiveDTO> GetActiveChallenge(Guid challengeId);
+    Task<ChallengeActiveDTO> GetActiveChallenge(Challenge challenge, Guid? guid);
+    Task<List<ChallengePlayerResultDTO>> GetResult(Guid challengeId);
+    Task<ChallengeQuestionDTO?> GetQuestionById(Guid questionId);
+    Task<int?> GetLastQuestionPoints(Guid challengeId, Guid questionId, Guid userId);
+}
